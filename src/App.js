7a86cce5e5644cd40b0677819d9eee5f5ed46e76
Gunjan <gunjan.garg@vinsol.com>
2023-02-23 12:18:50 +0530

    add global option

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.css';
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from 'highcharts';
-//import HC_more from "highcharts/highcharts-more";
 import { invertedBarOptions } from './components/inverted-bar/options';
 import { barOptions } from './components/bar/options';
-//HC_more(Highcharts);
 
 Highcharts.setOptions({
   chart: {
      style: {
-      fontFamily : "arial"
+      fontFamily : "serif"
      }
   }
 });
